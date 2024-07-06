77a792139e3ec752a5436d1749a97364a159f80a
bokdong <altmen77@naver.com>
2024-07-07 00:48:13 +0900

    07070047 김현우 - 채팅 UI 수정

--- app/src/main/java/com/khw/computervision/AppNavigator.kt
+++ app/src/main/java/com/khw/computervision/AppNavigator.kt
@@ -30,6 +30,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.NavType
